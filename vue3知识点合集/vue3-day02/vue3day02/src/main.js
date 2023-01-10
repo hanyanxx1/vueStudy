e7288706c5b1f5cc912a075bb8a34aa6a4851579
hanyanxx1 <hanyanxx1@163.com>
2023-01-10 19:19:08 +0800

    删除 Vue3 知识点合集

--- vue3知识点合集/vue3-day02/vue3day02/src/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-
-const vm = createApp(App).mount("#app");
-console.log(vm.message);
--- vue3知识点合集/vue3-day04/vueRouter/src/views/ShopTop.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h2>shop头部</h2>
-    </div>
-</template>
