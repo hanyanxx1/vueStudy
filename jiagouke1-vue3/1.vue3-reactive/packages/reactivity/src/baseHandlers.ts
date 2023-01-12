67f02e0331fa3ff05381bde854b7de760149f3eb
hanyanxx1 <hanyanxx1@163.com>
2023-01-13 05:10:03 +0800

    230113 - 7.触发更新

--- jiagouke1-vue3/1.vue3-reactive/packages/reactivity/src/baseHandlers.ts
+++ jiagouke1-vue3/1.vue3-reactive/packages/reactivity/src/baseHandlers.ts
@@ -1,7 +1,14 @@
 // 实现 new Proxy(target, handler)
-import { extend, isObject } from "@vue/shared";
-import { track } from "./effect";
-import { TrackOpTypes } from "./operators";
+import {
+  extend,
+  hasChanged,
+  hasOwn,
+  isArray,
+  isIntegerKey,
+  isObject,
+} from "@vue/shared";
+import { track, trigger } from "./effect";
+import { TrackOpTypes, TriggerOrTypes } from "./operators";
 import { reactive, readonly } from "./reactive";
 
 const get = createGetter();
@@ -52,6 +59,8 @@ function createGetter(isReadonly = false, shallow = false) {
     const res = Reflect.get(target, key, receiver);
     if (!isReadonly) {
       // 收集依赖，等会数据变化后更新对应的视图
+      // console.log("执行effect时会取值", "收集effect:", "key:" + key);
+
       track(target, TrackOpTypes.GET, key);
     }
     if (shallow) {
@@ -67,7 +76,26 @@ function createGetter(isReadonly = false, shallow = false) {
 function createSetter(shallow = false) {
   // 拦截设置功能
   return function set(target, key, value, receiver) {
+    const oldValue = target[key];
+
+    let hadKey =
+      isArray(target) && isIntegerKey(key)
+        ? Number(key) < target.length
+        : hasOwn(target, key);
+
     const result = Reflect.set(target, key, value, receiver);
+
+    if (!hadKey) {
+      // 新增
+      trigger(target, TriggerOrTypes.ADD, key, value);
+    } else if (hasChanged(oldValue, value)) {
+      // 修改
+      trigger(target, TriggerOrTypes.SET, key, value, oldValue);
+    }
+
+    // 我们要区分是新增的 还是修改的  vue2 里无法监控更改索引，无法监控数组的长度变化  -》 hack的方法 需要特殊处理
+
+    // 当数据更新时 通知对应属性的effect重新执行
 
     return result;
   };
