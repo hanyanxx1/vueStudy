97fa40a482f724f5f96b627eb66337f9a9038b71
hanyanxx1 <hanyanxx1@163.com>
2023-01-18 01:08:19 +0800

    230118 - 任务19：5.组件创建流程 、任务20：6.组件render函数调用关系 1、在html页将App组件，和prop通过createApp()传递进去 2、内部会根据App组件创建vnode 3、解析并调用App组件内部的setup 和 render方法并调用

--- jiagouke1-vue3/4.vue3-render/packages/runtime-core/src/renderer.ts
+++ jiagouke1-vue3/4.vue3-render/packages/runtime-core/src/renderer.ts
@@ -1,8 +1,45 @@
+import { ShapeFlags } from "@vue/shared";
 import { createAppAPI } from "./apiCreateApp";
+import { createComponentInstance, setupComponent } from "./component";
 
 export function createRenderer(rendererOptions) {
+  const setupRenderEfect = (instance) => {
+    // 需要创建一个effect 在effect中调用 render方法，这样render方法中拿到的数据会收集这个effect，属性更新时effect会重新执行
+    instance.render();
+  };
+  const mountComponent = (initialVNode, container) => {
+    // 组件的渲染流程  最核心的就是调用 setup拿到返回值，获取render函数返回的结果来进行渲染
+    // 1.先有实例
+    const instance = (initialVNode.component =
+      createComponentInstance(initialVNode));
+    // 2.需要的数据解析到实例上
+    setupComponent(instance);
+    // 3.创建一个effect 让render函数执行
+    setupRenderEfect(instance);
+  };
+  const processComponent = (n1, n2, container) => {
+    if (n1 == null) {
+      // 组件没有上一次的虚拟节点
+      mountComponent(n2, container);
+    } else {
+      // 组件更新流程
+    }
+  };
   // 告诉core 怎么渲染
-  const render = (vnode, container) => {};
+  const patch = (n1, n2, container) => {
+    // 针对不同类型 做初始化操作
+    const { shapeFlag } = n2;
+    if (shapeFlag & shapeFlag.ELEMENT) {
+    } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+      processComponent(n1, n2, container);
+    }
+  };
+  const render = (vnode, container) => {
+    // core的核心, 根据不同的虚拟节点 创建对应的真实元素
+
+    // 默认调用render 可能是初始化流程
+    patch(null, vnode, container);
+  };
 
   return {
     createApp: createAppAPI(render),
