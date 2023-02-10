ec998982505e9eb2de7aecb05b3e9554076bfd80
hanyanxx1 <hanyanxx1@163.com>
2023-02-10 21:19:50 +0800

    230210 - 06-实现 effect & reactive & 依赖收集 & 触发依赖

---
+++ guide-mini-vue/src/reactivity/effect.ts
@@ -0,0 +1,49 @@
+let activeEffect = void 0;
+const targetMap = new WeakMap();
+
+class ReactiveEffect {
+  private _fn: any;
+  constructor(fn) {
+    this._fn = fn;
+  }
+
+  run() {
+    // 执行的时候给全局的 activeEffect 赋值
+    // 利用全局属性来获取当前的 effect
+    activeEffect = this as any;
+    this._fn();
+  }
+}
+
+export function track(target, key) {
+  // 1. 先基于 target 找到对应的 dep
+  // 如果是第一次的话，那么就需要初始化
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    // 初始化 depsMap 的逻辑
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+
+  let dep = depsMap.get(key);
+  if (!dep) {
+    dep = new Set();
+    depsMap.set(key, dep);
+  }
+
+  dep.add(activeEffect);
+}
+
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  const dep = depsMap.get(key);
+
+  for (const effect of dep) {
+    effect.run();
+  }
+}
+
+export function effect(fn) {
+  const _effect = new ReactiveEffect(fn);
+  _effect.run();
+}
