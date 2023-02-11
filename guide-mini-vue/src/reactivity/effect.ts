77f847a7325e0aee93ab23b6db900902d29f3bd9
hanyanxx1 <hanyanxx1@163.com>
2023-02-11 21:02:27 +0800

    230211 - 09-实现 effect 的 stop 功能

--- guide-mini-vue/src/reactivity/effect.ts
+++ guide-mini-vue/src/reactivity/effect.ts
@@ -1,8 +1,13 @@
+import { extend } from "../shared/src";
+
 let activeEffect = void 0;
 const targetMap = new WeakMap();
 
 class ReactiveEffect {
   private _fn: any;
+  active = true;
+  deps = [];
+  onStop?: () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -12,6 +17,18 @@ class ReactiveEffect {
     // 利用全局属性来获取当前的 effect
     activeEffect = this as any;
     return this._fn();
+  }
+
+  stop() {
+    if (this.active) {
+      // 如果第一次执行 stop 后 active 就 false 了
+      // 这是为了防止重复的调用，执行 stop 逻辑
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
   }
 }
 
@@ -31,7 +48,10 @@ export function track(target, key) {
     depsMap.set(key, dep);
   }
 
+  if(!activeEffect) return;
+
   dep.add(activeEffect);
+  (activeEffect as any).deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -47,8 +67,24 @@ export function trigger(target, key) {
   }
 }
 
-export function effect(fn, options:any = {}) {
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+}
+
+export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler);
+
+  Object.assign(_effect, options);
+  extend(_effect, options);
   _effect.run();
-  return _effect.run.bind(_effect);
+
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
