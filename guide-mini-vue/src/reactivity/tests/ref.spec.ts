87541b5292b9cf669799a92dd7923f38bf08366a
hanyanxx1 <hanyanxx1@163.com>
2023-02-12 23:18:14 +0800

    230212 - 18-实现 proxyRefs 功能

--- guide-mini-vue/src/reactivity/tests/ref.spec.ts
+++ guide-mini-vue/src/reactivity/tests/ref.spec.ts
@@ -1,13 +1,8 @@
 import { effect } from "../effect";
 import { reactive } from "../reactive";
-import { isRef, ref, unRef } from "../ref";
+import { isRef, ref, unRef, proxyRefs } from "../ref";
 
 describe("ref", () => {
-  it("happy path", () => {
-    const a = ref(1);
-    expect(a.value).toBe(1);
-  });
-
   it("should be reactive", () => {
     const a = ref(1);
     let dummy;
@@ -38,6 +33,25 @@ describe("ref", () => {
     expect(dummy).toBe(1);
     a.value.count = 2;
     expect(dummy).toBe(2);
+  });
+
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name: "xiaohong",
+    };
+    const proxyUser = proxyRefs(user);
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe("xiaohong");
+
+    (proxyUser as any).age = 20;
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
   });
 
   it("isRef", () => {
