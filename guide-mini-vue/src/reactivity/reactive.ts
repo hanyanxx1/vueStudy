021fdaf087fb769bd443b1c872b3f298d8707895
hanyanxx1 <hanyanxx1@163.com>
2023-02-12 17:49:43 +0800

    230212 - 16-实现 ref 功能

--- guide-mini-vue/src/reactivity/reactive.ts
+++ guide-mini-vue/src/reactivity/reactive.ts
@@ -21,11 +21,19 @@ export function shallowReadonly(target) {
   return createReactiveObject(target, shallowReadonlyHandlers);
 }
 
+export function isProxy(value) {
+  return isReactive(value) || isReadonly(value);
+}
+
 export function isReadonly(value) {
   return !!value[ReactiveFlags.IS_READONLY];
 }
 
 export function isReactive(value) {
+  // 如果 value 是 proxy 的话
+  // 会触发 get 操作，而在 createGetter 里面会判断
+  // 如果 value 是普通对象的话
+  // 那么会返回 undefined ，那么就需要转换成布尔值
   return !!value[ReactiveFlags.IS_REACTIVE];
 }
 
