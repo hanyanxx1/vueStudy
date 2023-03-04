51971216dd6a1eb6fefc1a3804f7de79496b05a0
hanyanxx1 <hanyanxx1@163.com>
2023-03-04 22:52:36 +0800

    230304 - 2.布局组件

--- jiagouke1-vue3/10.vue3-ts-ui/packages/z-ui/index.ts
+++ jiagouke1-vue3/10.vue3-ts-ui/packages/z-ui/index.ts
@@ -2,7 +2,9 @@ import { App } from "vue";
 import Button from "@z-ui/button";
 import Icon from "@z-ui/icon";
 import ButtonGroup from "@z-ui/button-group";
-const components = [Button, Icon, ButtonGroup];
+import Row from "@z-ui/row";
+import Col from "@z-ui/col";
+const components = [Button, Icon, ButtonGroup, Row, Col];
 const install = (app: App): void => {
   components.forEach((component) => {
     app.component(component.name, component);
--- jiagouke1-vue3/10.vue3-ts-ui/website/App.vue
+++ jiagouke1-vue3/10.vue3-ts-ui/website/App.vue
@@ -17,6 +17,30 @@
       <i class="z-icon-arrow-right-bold"></i
     ></z-button>
   </z-button-group>
+  <hr />
+
+  <z-row>
+    <z-col :span="6"><div style="background: red">111</div></z-col>
+    <z-col :span="6" :offset="6"
+      ><div style="background: yellow">222</div></z-col
+    >
+    <z-col :span="6"><div style="background: pink">333</div></z-col>
+  </z-row>
+  <div style="border: 3px purple solid; width: 80%; margin: 0 auto">
+    <z-row :gutter="20">
+      <z-col :span="8"><div style="background: red">111</div></z-col>
+      <z-col :span="8"><div style="background: yellow">222</div></z-col>
+      <z-col :span="8"><div style="background: pink">333</div></z-col>
+    </z-row>
+  </div>
+  <div style="border: 3px purple solid; width: 80%; margin: 0 auto">
+    <z-row justify="space-around">
+      <z-col :span="2"><div style="background: red">111</div></z-col>
+      <z-col :span="2"><div style="background: yellow">222</div></z-col>
+      <z-col :span="2"><div style="background: pink">333</div></z-col>
+    </z-row>
+  </div>
+  <hr />
 </template>
 
 <script lang="ts">
